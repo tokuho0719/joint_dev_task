5d4dd958c8e14b866f806833c8e0bc2f94d2b65e
tokuho0719 <tokuho0719@yahoo.co.jp>
2021-02-24 22:32:59 +0900

    PHP応用21

--- task.php
+++ task.php
@@ -357,4 +357,20 @@ foreach ($humans as $human) {
 
 echo PHP_EOL;
 
+for($i = 1; $i <=30; $i++){
+  if($i === 15){
+    echo 'FizzBuzz' . PHP_EOL;
+  }elseif($i === 21){
+    echo 'FizzHoge' . PHP_EOL;
+  }elseif($i % 3 === 0){
+    echo 'Fizz' . PHP_EOL;
+  }elseif($i % 5 === 0){
+      echo 'Buzz' . PHP_EOL;
+  }elseif($i % 7 === 0){
+    echo 'Hoge' . PHP_EOL;
+  }else{
+    echo $i .PHP_EOL ;
+  }  
+}
+
 ?>
